22a6bd97cde52a236758489ced6aa55c1ba808b0
prashwar <prashwarmishra@gmail.com>
2020-06-01 15:25:59 +0530

    commit 2: Add more controllers and routes, install ejs and set views

--- codeial/index.js
+++ codeial/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.use('/', require('./routes'))
+app.use('/', require('./routes'));
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if (err){console.log(`There's been an error in connecting to the server: ${err}`); return;}
--- codeial/routes/index.js
+++ codeial/routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+router.use('/', require('./posts'));
+router.use('/users', require('./posts'));
 
 module.exports = router;
