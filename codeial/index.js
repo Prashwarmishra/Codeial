a05cf7b9afb0ca8a31830a3a6d94c4130354823d
prashwar <prashwarmishra@gmail.com>
2020-06-01 14:54:25 +0530

    commit 1: add a controller and a router for home

---
+++ codeial/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.use('/', require('./routes'))
+
+app.listen(port, function(err){
+    if (err){console.log(`There's been an error in connecting to the server: ${err}`); return;}
+    console.log(`Yup, the server is Up and Running at the port: ${port}`);
+})
