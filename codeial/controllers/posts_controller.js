20ad0c657b3d70951b7c1370babb96357a155c64
prashwar <prashwarmishra@gmail.com>
2020-06-12 15:01:55 +0530

    commit 14: Add Delete Button using an anchor tag

--- codeial/controllers/posts_controller.js
+++ codeial/controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -7,5 +8,20 @@ module.exports.create = function(req, res){
     }, function(err, post){
         if (err){console.log("Error in posting the status."); return;}
         return res.redirect('back');
+    })
+}
+
+module.exports.destory = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if (err){console.log("There's an error while trying to delete the comment"); return;}
+        if (post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if (err){console.log("There was an error while trying to delete the post"); return;}
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
     })
 }
